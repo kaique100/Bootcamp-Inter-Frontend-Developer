5bd26968433dd0255e2ed1e5ca2498dd2473d7b8
Kaique Santos <skaique5424@gmail.com>
2021-12-21 15:48:40 -0300

    finalizado o curso de Sintaxe e Operadores

---
+++ Sintaxe e Operadores/comparaNumeros.js
@@ -0,0 +1,47 @@
+var comparaNumeros = (num1, num2) => {
+    // const saoIguais = num1 === num2;
+    // const soma = num1 + num2;
+
+    // if (saoIguais) {
+    //     return "Sao iguais";
+    // }
+
+    // return saoIguais ? "São iguais" : "não são iguais";
+
+    const pirmeiraFrase = criaPrimeiraFrase(num1, num2);
+    const segundaFrase = criaSegundaFrase(num1, num2);
+
+    return `${pirmeiraFrase} ${segundaFrase}`;
+}
+
+function criaPrimeiraFrase(num1, num2) {
+    let saoIguais = "";
+
+    if (num1 !== num2) {
+        saoIguais = "não";
+    }
+
+    return `Os números ${num1} e ${num2} ${saoIguais} são iguais.`;
+}
+
+function criaSegundaFrase(num1, num2){
+    const soma = num1 + num2;
+
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+    if (compara10) {
+        resultado10 = 'maior';
+    }
+
+    if (compara20) {
+        resultado20 = 'maior';
+    }
+
+    return `Sua soma é ${soma},  que é ${resultado10} que 10 e ${resultado20} que 20.`;
+}
+
+console.log(comparaNumeros(2, 20));
