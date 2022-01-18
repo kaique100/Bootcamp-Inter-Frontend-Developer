923155a56120551d10d07e91945abd88922ea08d
Kaique Santos <skaique5424@gmail.com>
2022-01-18 15:00:01 -0300

    finalizado o curso de Funcoes e o curso de variaveis e tipos

---
+++ Variáveis e Tipos/index.js
@@ -0,0 +1,30 @@
+// Variável
+
+// let numberOne;
+// numberOne = 1;
+
+// console.log(numberOne + 2);
+
+// var numberOne;
+
+// var firstName = 'João';
+// var lastName = 'Souza';
+
+// if (firstName === 'João') {
+//     var firstName = "Pedro";
+//     let lastName = "Silva";
+// }
+
+// console.log(firstName, lastName);
+
+// Constante
+
+// const fIRST_NAME = "Kaique";
+
+// const fIRST_NAME = "João";
+
+const FISRT_NAME = "Kaique";
+
+// FISRT_NAME = "Kaique";
+
+console.log(FISRT_NAME);
